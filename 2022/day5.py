9caa5bcdf197ebf1a37770bd5a8add7c408f969f
ColinHDev <colinheidfeld@gmail.com>
2022-12-05 15:07:54 +0100

    no longer hardcoding the stacks

--- 2022/day5.py
+++ 2022/day5.py
@@ -1,4 +1,4 @@
-crates = [
+"""crates = [
     ["T", "P", "Z", "C", "S", "L", "Q", "N"],
     ["L", "P", "T", "V", "H", "C", "G"],
     ["D", "C", "Z", "F"],
@@ -8,22 +8,32 @@ crates = [
     ["V", "W", "G", "B", "D"],
     ["N", "J", "S", "Q", "H", "W"],
     ["R", "C", "Q", "F", "S", "L", "V"]
-]
+]"""
+crates = []
 
 
 def move_crate(amount, fromindex, toindex):
-    popIndex = len(crates[fromindex]) - amount
+    popindex = len(crates[fromindex]) - amount
     while amount > 0:
         crates[toindex].append(crates[fromindex][len(crates[fromindex]) - amount])
-        crates[fromindex].pop(popIndex)
+        crates[fromindex].pop(popindex)
         amount -= 1
 
 
 for line in open("input.txt", "r"):
-    if not line.startswith("move "):
-        continue
-    lineParts = line.split(" ")
-    move_crate(int(lineParts[1]), int(lineParts[3]) - 1, int(lineParts[5]) - 1)
+    if line.startswith("["):
+        stackCount = int((len(line) + 1) / 4)
+        for i in range(stackCount):
+            char = line[i * 4 + 1]
+            if not char.isalpha():
+                continue
+            while len(crates) <= i:
+                crates.append([])
+            crates[i].insert(0, char)
+
+    elif line.startswith("move"):
+        lineParts = line.split(" ")
+        move_crate(int(lineParts[1]), int(lineParts[3]) - 1, int(lineParts[5]) - 1)
 
 for stack in crates:
     print(stack[-1], end="")
