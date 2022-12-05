d817b23276c4bbd32b5fbe7aa239bfaa2e175784
ColinHDev <colinheidfeld@gmail.com>
2022-12-05 09:53:41 +0100

    second star

--- 2022/day4.py
+++ 2022/day4.py
@@ -3,8 +3,7 @@ for line in open("input.txt", "r"):
     [input1, input2] = line.split(",")
     pair1 = input1.split("-")
     pair2 = input2.split("-")
-    if int(pair1[0]) <= int(pair2[0]) and int(pair1[1]) >= int(pair2[1]):
-        inclusions += 1
-    elif int(pair1[0]) >= int(pair2[0]) and int(pair1[1]) <= int(pair2[1]):
-        inclusions += 1
+    if int(pair1[1]) < int(pair2[0]) or int(pair1[0]) > int(pair2[1]):
+        continue
+    inclusions += 1
 print(inclusions)
