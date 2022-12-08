c48c1953c3bf51016b8f14be111a65418588dae4
ColinHDev <colinheidfeld@gmail.com>
2022-12-08 09:58:56 +0100

    second star

--- 2022/day8.py
+++ 2022/day8.py
@@ -3,41 +3,41 @@ for line in open("input.txt", "r"):
     trees.append(line.replace("\n", ""))
 
 
-def is_tree_visible(row: int, column: int):
+def calculate_scenic_score(row: int, column: int):
     # edge tree
     if row == 0 or row == len(trees) - 1 or column == 0 or column == len(trees[row]) - 1:
-        return True
+        return 0
     height = int(trees[row][column])
     # looking up
-    for i in range(0, row):
+    up_count = 0
+    for i in range(row - 1, -1, -1):
+        up_count += 1
         if int(trees[i][column]) >= height:
             break
-    else:
-        return True
     # looking down
+    down_count = 0
     for i in range(row + 1, len(trees)):
+        down_count += 1
         if int(trees[i][column]) >= height:
             break
-    else:
-        return True
     # looking left
-    for i in range(0, column):
+    left_count = 0
+    for i in range(column - 1, -1, -1):
+        left_count += 1
         if int(trees[row][i]) >= height:
             break
-    else:
-        return True
     # looking right
+    right_count = 0
     for i in range(column + 1, len(trees[row])):
+        right_count += 1
         if int(trees[row][i]) >= height:
             break
-    else:
-        return True
-    return False
+    return up_count * down_count * left_count * right_count
 
 
-visible_tree_count = 0
-for row_ in range(0, len(trees)):
-    for column_ in range(0, len(trees[row_])):
-        if is_tree_visible(row_, column_):
-            visible_tree_count += 1
-print(visible_tree_count)
+highest_scenic_score = 0
+for row_ in range(1, len(trees) - 1):
+    for column_ in range(1, len(trees[row_]) - 1):
+        score = calculate_scenic_score(row_, column_)
+        highest_scenic_score = max(score, highest_scenic_score)
+print(highest_scenic_score)
